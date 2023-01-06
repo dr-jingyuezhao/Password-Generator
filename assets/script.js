df30a1e7cdff208cf54bf8d74dc78015546b5a25
Jasmine <dr.jingyuezhao@outlook.com>
2023-01-06 20:59:55 +0000

    add function to check the password length and log to the console

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,5 @@
 // The Password generator will create a random password with 10-64 characters based on criteria the user specifies.
 
-// Variables declaration
-var passwordLength = 0;
-
 // Array of special characters to be included in password
 var specialCharacters = [
   "@",
@@ -93,33 +90,78 @@ var uppercaseCharacters = [
   "Z",
 ];
 
+// Variables declaration
+var passwordLength = 0;
+
+
 // define the passwordOptions object
 var passwordOptions = {
   // properties are made up of key-value pairs
-length: passwordLength,
-lowercase: lowercaseCharacters,
-uppercase: uppercaseCharacters,
-numeric: numericCharacters,
-specialChar: specialCharacters,
+  length: passwordLength,
+  hasLowercase: function() {
+    confirm("Please confirm OK if you would like to have lowercase in your password.");
+    return;
+  },
+  lowercase: lowercaseCharacters,
+  hasUppercase: function() {
+    confirm("Please confirm OK if you would like to have uppercase in your password.");
+  },
+  uppercase: uppercaseCharacters,
+  hasNumber: function() {
+    confirm("Please confirm OK if you would like to have number in your password.");
+  },
+  number: numericCharacters,
+  hasSpecial: function() {
+    confirm("Please confirm OK if you would like to have special characters in your password.");
+  },
+  special: specialCharacters,
 };
 
-console.log("Your password options are: ", passwordOptions);
-console.log(Object.entries(passwordOptions));
+// console.log("Your password options are: ", passwordOptions);
+// console.log(Object.entries(passwordOptions));
+
+//Functions for
+function lengthMessage() {
+  if (passwordLength < 10 || passwordLength > 64) {
+    alert(
+      "Password length must be at least 10 characters but no more than 64."
+    );
+    console.log(
+      "The number of password length " + passwordLength + " is invalid."
+    );
+    passwordLength = prompt("Please try again and enter a number between 10 and 64.");
+  } else {
+    alert("Your password will have " + passwordLength + " characters.");
+  }
+  console.log("Your password length is: " + passwordLength);
+}
 
 // Function to prompt user for password options
 function getPasswordOptions() {
   // prompt the user for the length of password
-  passwordLength = prompt("Please choose the length of your password: enter a number between 10 and 64.");
+  passwordLength = prompt(
+    "Please choose the length of your password: enter a number between 10 and 64."
+  );
   // alert if the password length is <10 or >64
-  if (passwordLength < 10 || passwordLength > 64) {
-    alert("Password length must be at least 10 characters but no more than 64.");
-  } else {
-    alert("Your password will have " + passwordLength + " characters.");
-  }
+  lengthMessage();
+
+  // prompt the user for character types to be included in the password
+  
+  // prompt the user for lowercase character to be included in the password
+  passwordOptions.hasLowercase();
+  console.log("Your password has lowercase: ", passwordOptions.hasLowercase());
+  // return hasLowercase;
+  // prompt the user for uppercase character to be included in the password
+  passwordOptions.hasUppercase();
+  // return hasUppercase;
+  passwordOptions.hasNumber();
+  passwordOptions.hasSpecial();
+
+
 }
 
-// call the function to get password length
-getPasswordOptions();
+// call the function to get password options
+var userInput = getPasswordOptions();
 
 // Function for getting a random element from an array
 function getRandom(arr) {}
